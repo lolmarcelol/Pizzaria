665ba24044a634d75c4de2cbf68306109f0f07b1
marce <marce@DESKTOP-NOR9PI2.Home>
2015-12-07 18:11:38 -0200

    atualizado

--- forumFinal/src/java/forum/controller/MensagemServlet.java
+++ forumFinal/src/java/forum/controller/MensagemServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import static jdk.nashorn.internal.runtime.Debug.id;
 
 
 public class MensagemServlet extends HttpServlet {
@@ -21,40 +22,45 @@ public class MensagemServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         int id;
         String idParam;
         idParam = request.getParameter("id");
         id = Integer.parseInt(idParam);
-        ITopicoManager Tmanager;
-        Tmanager = new TopicoManager();
-        Tmanager.incrementaAcesso(id);
         ArrayList<Mensagem> mensagens;
         IMensagemManager manager;
         manager = new MensagemManager();
         mensagens = manager.getMensagem(id);
-        request.setAttribute("mensagens",mensagens);
+        request.setAttribute("mensagens",mensagens); 
+        ITopicoManager Tmanager;
+        Tmanager = new TopicoManager();
+        Tmanager.incrementaAcesso(id);
         RequestDispatcher rd;
         rd = request.getRequestDispatcher("/WEB-INF/jsp/mensagem.jsp");
         rd.forward(request, response);
-    }
+}
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       Mensagem mensagem = new Mensagem();
-       mensagem.setText(request.getParameter("text")); // parametro que esta no campo name
-       
-       String idaux;
-       idaux = request.getParameter("id");
-       int id_topico =Integer.parseInt(idaux);
-       mensagem.setTopico_id(id_topico);
-       mensagem.setUsuario(request.getParameter("usuario"));
-       Date data = new Date();
-       mensagem.setData(data);
-       MensagemManager manager;
-       manager = new MensagemManager();
-       manager.criarMensagem(mensagem);
-       manager.incrementaMensagem(id_topico);
+        Mensagem mensagem = new Mensagem();
+        mensagem.setText(request.getParameter("text")); // parametro que esta no campo name
+        String idaux;
+        idaux = request.getParameter("id");
+        int id_topico =Integer.parseInt(idaux);
+        mensagem.setTopico_id(id_topico);
+        mensagem.setUsuario(request.getParameter("usuario"));
+        Date data = new Date();
+        mensagem.setData(data);
+        MensagemManager manager;
+        manager = new MensagemManager();
+        manager.criarMensagem(mensagem);
+        manager.incrementaMensagem(id_topico);
+        ArrayList<Mensagem> mensagens;
+        manager = new MensagemManager();
+        mensagens = manager.getMensagem(id_topico);
+        request.setAttribute("mensagens",mensagens); 
+        RequestDispatcher rd;
+        rd = request.getRequestDispatcher("/WEB-INF/jsp/mensagem.jsp");
+        rd.forward(request, response);
     }
 }
